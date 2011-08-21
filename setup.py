8a050fa8cf30fdb4b3608a76c2f0c831408d7d66
Dyogo Ribeiro Veiga <dyogo.nsi@gmail.com>
2011-08-21 14:16:40 -0300

    Modified setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
+import os
 
 version = "0.0.1"
 readme = open("README.rst").read()
 
+os.system("msgfmt chesstournament/interface/locales/en/LC_MESSAGES/menu.po -o chesstournament/interface/locales/en/LC_MESSAGES/menu.mo")
+os.system("msgfmt chesstournament/interface/locales/pt_BR/LC_MESSAGES/menu.po -o chesstournament/interface/locales/pt_BR/LC_MESSAGES/menu.mo")
 setup(name="chesstournament",
       version=version,
       description="Play chess with your friends",
@@ -12,3 +15,5 @@ setup(name="chesstournament",
       author_email="angelonuffer@gmail.com",
       packages=["chesstournament"],
       )
+      
+
