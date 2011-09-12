980fb1d71e274d8698b2a93e3449c17ba05418b4
Ângelo Otávio Nuffer Nunes <angelonuffer@gmail.com>
2011-09-12 20:24:56 -0300

    center menu buttons

--- sheldonchess/interface/web/sheldonchess.py
+++ sheldonchess/interface/web/sheldonchess.py
@@ -1,26 +1,26 @@
-from rajesh import Application, run
+from rajesh import Application, run, expr
 from rajesh.element import Img, Button
 
 
 class SheldonChess(Application):
 
     def begin(self):
-        background = Img(id="background", src="images/Image_chessSound.jpg", width=800, height=600)
+        background = Img(id="background", src="images/Image_chessSound.jpg", width="100%", height="100%")
         self.put(background, (0, 0))
 
         new_game = Button(id="new_game")
         new_game.text = "New game"
-        self.put(new_game, (300, 420))
+        self.put(new_game, (expr("document.width / 2 - new_game.offsetWidth / 2"), expr("document.height / 2 + 20")))
         self.js.new_game.onclick = self.new_game
 
         options = Button(id="options")
         options.text = "Options"
-        self.put(options, (340, 420))
+        self.put(options, (expr("document.width / 2 - options.offsetWidth / 2"), expr("document.height / 2 + 60")))
         self.js.options.onclick = self.options
 
         credits = Button(id="credits")
         credits.text = "Credits"
-        self.put(credits, (380, 420))
+        self.put(credits, (expr("document.width / 2 - credits.offsetWidth / 2"), expr("document.height / 2 + 100")))
         self.js.credits.onclick = self.credits
 
     def new_game(self):
