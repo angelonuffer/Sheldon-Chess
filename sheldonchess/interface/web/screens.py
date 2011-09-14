ad2c158beb0e6b237fda4c29282442965e2be798
Ângelo Otávio Nuffer Nunes <angelonuffer@gmail.com>
2011-09-14 19:58:32 -0300

    improve screen style

--- sheldonchess/interface/web/screens.py
+++ sheldonchess/interface/web/screens.py
@@ -9,11 +9,24 @@ class Screen(Div):
         super(Screen, self).__init__(**kwargs)
         self.app = app
 
+    @property
+    def js(self):
+        return getattr(self.app.js, self.parameters["id"])
+
+    def on_put(self):
+        self.js.style.setProperty("margin-left", expr("-%s.offsetWidth / 2" % self.parameters["id"]))
+        self.js.style.setProperty("margin-top", expr("-%s.offsetHeight / 2" % self.parameters["id"]))
+        self.js.style.setProperty("background", "#aaaaaa")
+        self.js.style.setProperty("border-radius", 9)
+        self.js.style.setProperty("border-style", "solid")
+        self.js.style.setProperty("padding", 5)
+
 
 class Menu(Screen):
 
     def __init__(self, app, buttons, **kwargs):
         super(Menu, self).__init__(app, **kwargs)
+        self.buttons = buttons
         for button_name, action in buttons:
             button_id = button_name.lower().replace(" ", "_")
             button = Button(id=button_id, onclick="sock.send('%s')" % action.__name__)
--- sheldonchess/interface/web/sheldonchess.py
+++ sheldonchess/interface/web/sheldonchess.py
@@ -9,7 +9,7 @@ class SheldonChess(Application):
         background = Img(id="background", src="images/sheldonchess_background.png", width="100%", height="100%")
         self.put(background, (0, 0))
         main_menu = MainMenu(self)
-        self.put(main_menu, (expr("document.width / 2 - main_menu.offsetWidth / 2"), expr("document.height / 2 - main_menu.offsetHeight / 2")))
+        self.put(main_menu, ("50%", "50%"))
 
 
 if __name__ == "__main__":
